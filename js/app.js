e28975d9416733bee7cd429de8003e73c4fd3ed1
Petar Petkov <petar.b.petkov@gmail.com>
2014-11-20 02:52:01 +0200

    UpdateLavesl function added, isGameover and a lot more

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
     //Game configuration
     var context;
     var queue;
-    var WIDTH = Math.floor(($(window).width())*0.996);
-    var HEIGHT = Math.floor(($(window).height())*0.996);
+    var WIDTH = Math.floor(($(window).width()) * 0.996);
+    var HEIGHT = Math.floor(($(window).height()) * 0.996);
     var mouseXPosition;
     var mouseYPosition;
     var stage;
@@ -20,7 +20,7 @@ $(document).ready(function () {
 
     var yearLength = 1000;
     var gameTime = 1960;
-    var winLevel = 1970;
+    var winLevel = 2000;
     var gameFPS = 60;
     var minGameSpeed = 1;
     var maxGameSpeed = 10;
@@ -28,33 +28,35 @@ $(document).ready(function () {
     var player;
     var hitPoints = 500;
     // levels 60, 70 , 80 , 90, 2000, 2010, 2020
-    var levelsGoals = {'1960':1000, 
-                       '1970':2000,
-                       '1980':3000,
-                       '1990':4000,
-                       '2000':5000,
-                       '2010':6000 
-     };
-     
+    var levelsGoals = [ 0, 1000, 2000, 3000, 4000, 5000, 6000];
+    var levels = [1960, 1970, 1980, 1990, 2000, 2010, 2020]
+
 
     //initial game setup
-    window.onload = function () {
-        
+    window.onload = function() {
+
         //Setup canvas with height and size of the screen
 
         var canvas = document.getElementById('stage');
         context = canvas.getContext('2d');
         context.canvas.width = WIDTH;
         context.canvas.height = HEIGHT;
+//        $('#playground').css('height', HEIGHT + 100);
+//        //$('#playground').css('background-image', 'images/background/1975.png');
+//        $('#avatar').css({
+//            'top': (HEIGHT-50)+'px',
+//            'left': (WIDTH /2-75) + 'px',
+//            'src': 
+//        });
         stage = new createjs.Stage("stage");
-                
+
         //Setup the Asset Queue and load sounds
-        
+
         queue = new createjs.LoadQueue(false);
         queue.installPlugin(createjs.Sound);
         queue.on("complete", queueLoaded, this);
         createjs.Sound.alternateExtensions = ["ogg"];
-       
+
         //Create a load manifest for all assets
 
         queue.loadManifest([
@@ -67,13 +69,13 @@ $(document).ready(function () {
             {id: 'player', src: 'images/players/bChervenkov.png'},
             //{id: 'tick', src: 'sound/tick.mp3'},
             {id: 'gameOverSound', src: 'sound/gameOver.mp3'}//,
-           // {id: 'shot', src: '../sound/shot.mp3'},
-           // {id: 'background', src: '../sound/countryside.mp3'},
+            // {id: 'shot', src: '../sound/shot.mp3'},
+            // {id: 'background', src: '../sound/countryside.mp3'},
 
-           // {id: 'deathSound', src: '../sound/die.mp3'}
+            // {id: 'deathSound', src: '../sound/die.mp3'}
         ]);
         queue.load();
-       
+
 
         //Create a timer that updates once every 5 seconds
 
@@ -83,7 +85,7 @@ $(document).ready(function () {
 
     //load all resources
 
-    function queueLoaded(event){
+    function queueLoaded(event) {
 
         // Add background image
 
@@ -103,6 +105,13 @@ $(document).ready(function () {
         scoreText.x = 10;
         scoreText.y = 10;
         stage.addChild(scoreText);
+        
+        //Add Score Label to the stage
+
+        levelGoalText = new createjs.Text("Level goal: " + 1000, "36px Arial", "#FFF");
+        levelGoalText.x = 10;
+        levelGoalText.y = 50;
+        stage.addChild(levelGoalText);
 
         //Ad Timer to the scene
 
@@ -110,11 +119,6 @@ $(document).ready(function () {
         timerText.x = WIDTH - 250;
         timerText.y = 10;
         stage.addChild(timerText);
-        
-        //Game Over Label and not added
-        gameOverText = new createjs.Text("GAME OVER", "56px Arial", "#FFF");
-        gameOverText.x = WIDTH/2 - 180;
-        gameOverText.y = HEIGHT/2 - 15;
 
         // Play background sound to the scene
 
@@ -125,15 +129,15 @@ $(document).ready(function () {
         spriteSheet = new createjs.SpriteSheet({
             "images": [queue.getResult('batSpritesheet')],
             "frames": {"width": 150, "height": 150},
-            "animations": { "flap": [0,0] }
+            "animations": {"flap": [0, 0]}
         });
 
         // Create enemy explosion spritesheet
 
         deadEnemySheet = new createjs.SpriteSheet({
             "images": [queue.getResult('enemyExplosion')],
-            "frames": {"width": 201, "height" : 201},
-            "animations": {"die": [0,12, false,1 ] }
+            "frames": {"width": 201, "height": 201},
+            "animations": {"die": [0, 12, false, 1]}
         });
 
         // Create first enemy
@@ -159,7 +163,7 @@ $(document).ready(function () {
 
     }
 
-    function createEnemy(){
+    function createEnemy() {
         enemyXPos = getNewX();
         enemyYPos = getNewY();
         enemyXSpeed *= randomSign();
@@ -170,30 +174,30 @@ $(document).ready(function () {
         animation.x = enemyXPos;
         animation.y = enemyYPos;
         animation.gotoAndPlay("flap");
-        stage.addChildAt(animation,1);
+        stage.addChildAt(animation, 1);
     }
 
     //Random generators
-    function getNewX(){
-        var newX = Math.abs(Math.floor((Math.random()*((WIDTH-74)-75)+75)));
+    function getNewX() {
+        var newX = Math.abs(Math.floor((Math.random() * ((WIDTH - 74) - 75) + 75)));
         return newX;
     }
 
-    function getNewY(){
-        var newY = Math.abs(Math.floor((Math.random()*((HEIGHT-74)-75)+75)));
+    function getNewY() {
+        var newY = Math.abs(Math.floor((Math.random() * ((HEIGHT - 74) - 75) + 75)));
         return newY;
     }
 
-    function randomSign(){
-        var num = Math.floor(Math.random()*2);
+    function randomSign() {
+        var num = Math.floor(Math.random() * 2);
         if (num == 0) {
             return -1;
-        }else{
+        } else {
             return 1;
         }
     }
 
-    function enemyDeath(){
+    function enemyDeath() {
         deathAnimation = new createjs.Sprite(deadEnemySheet, "die");
         deathAnimation.regX = 100;
         deathAnimation.regY = 100;
@@ -206,13 +210,13 @@ $(document).ready(function () {
     function tickEvent()
     {
         //Make sure enemy bat is within game boundaries and move enemy Bat
-        if(enemyXPos <= WIDTH - 75 && enemyXPos >= 74){
+        if (enemyXPos <= WIDTH - 75 && enemyXPos >= 74) {
             enemyXPos += enemyXSpeed;
         } else {
             enemyXSpeed = enemyXSpeed * (-1);
             enemyXPos += enemyXSpeed;
         }
-        if(enemyYPos <= HEIGHT - 75 && enemyYPos >= 74) {
+        if (enemyYPos <= HEIGHT - 75 && enemyYPos >= 74) {
             enemyYPos += enemyYSpeed;
         } else {
             enemyYSpeed = enemyYSpeed * (-1);
@@ -241,10 +245,10 @@ $(document).ready(function () {
     }
 
 
-    function handleMouseMove(event){
+    function handleMouseMove(event) {
         //Offset the position by 45 pixels so mouse is in center of crosshair
-        crossHair.x = event.clientX-45;
-        crossHair.y = event.clientY-45;
+        crossHair.x = event.clientX - 45;
+        crossHair.y = event.clientY - 45;
     }
 
     function handleMouseDown(event) {
@@ -267,15 +271,15 @@ $(document).ready(function () {
         var distY = Math.abs(shotY - spriteY);
 
         // Anywhere in the body or head is a hit - but not the wings
-        if(distX < 60 && distY < 75 ) {
+        if (distX < 60 && distY < 75) {
             //Hit
             console.log(distX + " " + distY);
             stage.removeChild(animation);
             enemyDeath();
-            enemyXPos = -200 ;
+            enemyXPos = -200;
             enemyYPos = -200;
             score += hitPoints;
-            scoreText.text = "Score: " + score.toString();
+            
             createjs.Sound.play("deathSound");
 
             //Make it harder next time
@@ -283,8 +287,8 @@ $(document).ready(function () {
             enemyXSpeed *= 1.3;
 
             //Create new enemy
-            var timeToCreate = Math.floor((Math.random()*3500)+1);
-            setTimeout(createEnemy,timeToCreate);
+            var timeToCreate = Math.floor((Math.random() * 3500) + 1);
+            setTimeout(createEnemy, timeToCreate);
 
         } else {
 
@@ -294,25 +298,36 @@ $(document).ready(function () {
         }
     }
 
+//the Tick function
     function updateTime() {
         isGameover()
-        //updateLabels(); todo
- //update part
+        updateLabels(); 
         gameTime += 1;
-        timerText.text = "Year: " + gameTime;
         createjs.Sound.play("tick");
+    }
+
+//update all labels on screen
+    function updateLabels(){
+         levelGoalText.text = "Level goal: " + levelsGoals[levels.indexOf(Number(getCurrentLevel()))+1];
+         timerText.text = "Year: " + gameTime.toString();
+         scoreText.text = "Score: " + score.toString();
     }
 
 //End Game and Clean up the stage
 
-    function isGameover(){  
-        if(getCurrentLevel() === winLevel){
+    function isGameover() {
+        if (getCurrentLevel() === winLevel) {
+            gameOverText = new createjs.Text("You won! You are programmer now!", "56px Arial", "#FFF");
+            gameOverText.x = WIDTH / 2 - 450;
+            gameOverText.y = HEIGHT / 2 - 15;
             stage.addChild(gameOverText);
-            gameOverText.x -= 350;
-            //gameOverText.
-            gameOverText.text = 'You won! You are programmer now!'
+            stage.removeChild(animation);
+            stage.removeChild(crossHair);
             clearInterval(gameTimer);
-        } else if(false){
+        } else if (isLevelFailed()) {
+            gameOverText = new createjs.Text("Game Over", "56px Arial", "#FFF");
+            gameOverText.x = WIDTH / 2 - 150;
+            gameOverText.y = HEIGHT / 2 - 15;
             stage.addChild(gameOverText);
             stage.removeChild(animation);
             stage.removeChild(crossHair);
@@ -320,22 +335,30 @@ $(document).ready(function () {
             clearInterval(gameTimer);
         }
     }
-    function getCurrentLevel(){
+    function isLevelFailed(){
+        var levelGoal = levelsGoals[levels.indexOf(Number(getCurrentLevel()))];
+        
+        if(gameTime == getCurrentLevel() && score < levelGoal){
+            return true;
+        }
+        return false;
+    }
+    function getCurrentLevel() {
         var gameLevel;
-        if(gameTime >= 1960 && gameTime < 1970){
-            gameLevel=1960;
-        }else if(gameTime >= 1970 && gameTime < 1980){
-            gameLevel=1970;
-        }else if(gameTime >= 1980 && gameTime < 1990){
-            gameLevel=1980;
-        }else if(gameTime >= 1990 && gameTime < 2000){
-            gameLevel=1990;
-        }else if(gameTime >= 2000 && gameTime < 2010){
-            gameLevel=2000;
-        }else if(gameTime >= 2010 && gameTime < 2020){
-            gameLevel=2010;
-        }else if(gameTime >= 2010){
-            gameLevel=2020;
+        if (gameTime >= 1960 && gameTime < 1970) {
+            gameLevel = 1960;
+        } else if (gameTime >= 1970 && gameTime < 1980) {
+            gameLevel = 1970;
+        } else if (gameTime >= 1980 && gameTime < 1990) {
+            gameLevel = 1980;
+        } else if (gameTime >= 1990 && gameTime < 2000) {
+            gameLevel = 1990;
+        } else if (gameTime >= 2000 && gameTime < 2010) {
+            gameLevel = 2000;
+        } else if (gameTime >= 2010 && gameTime < 2020) {
+            gameLevel = 2010;
+        } else if (gameTime >= 2010) {
+            gameLevel = 2020;
         }
         return Number(gameLevel);
     }
